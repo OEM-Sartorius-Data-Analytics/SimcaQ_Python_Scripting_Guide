60dca467387bfbb57e26b67b6ec12166ee90c08f
Javier Sotres <jsotres@gmail.com>
2022-12-10 23:18:31 +0100

    c

--- 01_ProjectInterface/ExploreProjectInterface.py
+++ 01_ProjectInterface/ExploreProjectInterface.py
@@ -1,7 +1,20 @@
 from win32com import client as win32
+import argparse
 
 if __name__ == '__main__':
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--project", required=True, help="Path to the Simca project")
+    args = vars(ap.parse_args())
+
+    PathSimcaProject = args["project"]
     
     simcaq = win32.Dispatch('Umetrics.SIMCAQ')
 
     project = simcaq.OpenProject(PathSimcaProject, "")
+
+    print("You have loaded the project ", project.GetProjectName(), ", which has ", project.GetNumberOfModels(), " models.")
+
+    project.DisposeProject()
+
+
